8d5f056a386c117d00ce39f7538df8a4b4b3275d
hagi <fabiorook9000@gmail.com>
2026-01-19 18:07:28 +0100

    Estructura inicial: Arquitectura Hexagonal para modulo account

---
+++ src/test/java/com/banco_hexagona/bancoHexagonal/BancoHexagonalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.banco_hexagona.bancoHexagonal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BancoHexagonalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
